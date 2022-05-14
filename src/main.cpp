e976185beff87957e78ca332ddbbe4f7995effa8
Henrique Paulino Cruz <49167008+HenriqueCruz5341@users.noreply.github.com>
2022-05-14 14:43:21 -0300

    consertando main.c

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,7 @@
 #include <Arduino.h>
-#include <ultrasonic.h>
-#include <cup.h>
-
-const int trigPin = 5;
-const int echoPin = 18;
-
-//const int portaRele = 5;
-
-//Ultrasonic ultrasonic = Ultrasonic(trigPin, echoPin);
-Cup cup = Cup(trigPin, echoPin);
-
-void setup() {
-  //pinMode(portaRele, OUTPUT);
-  Serial.begin(115200); // Starts the serial communication
-}
-
-void loop() {
-  // Calculate the distance
-  // float distanceMm = ultrasonic.getDistanceMm();
-  #include <Arduino.h>
-#include <ultrasonic.h>
-#include <cup.h>
 #include <button.h>
+#include <cup.h>
+#include <ultrasonic.h>
 
 const int trigPinCup = 5;
 const int echoPinCup = 18;
@@ -39,70 +19,60 @@ int requestedCups = 5;
 const int mmToCups = 30;
 const int maxMm = 200;
 
-//const int portaRele = 5;
+// const int portaRele = 5;
 
-Ultrasonic ultrasonic = Ultrasonic(trigPinWat, echoPinWat);
-Cup cup = Cup(trigPinCup, echoPinCup);
-Button buttonRequest = Button(buttonRequestPin);
+// Ultrasonic ultrasonic = Ultrasonic(trigPinWat, echoPinWat);
+// Cup cup = Cup(trigPinCup, echoPinCup);
+// Button buttonRequest = Button(buttonRequestPin);
 Button buttonAddCup = Button(buttonAddCupPin);
 
 void setup() {
-  //pinMode(portaRele, OUTPUT);
-  pinMode(ledBuiltIn, OUTPUT);
-  Serial.begin(115200); // Starts the serial communication
+    // pinMode(portaRele, OUTPUT);
+    pinMode(ledBuiltIn, OUTPUT);
+    Serial.begin(115200);  // Starts the serial communication
 }
 
 void loop() {
-  // Calculate the distance
-  // float distanceMm = ultrasonic.getDistanceMm();
-  // bool hasCup = cup.checkCup();
-  
-  // if(hasCup){
-  //   Serial.println("Tem xícara");
-  //   while(actualQtdCup < requestedCups && hasCup){
-  //     digitalWrite(ledBuiltIn, HIGH);
-  //     Serial.print("Distance (mm): ");
-  //     Serial.println(distanceMm);
-      
-  //     actualQtdCup = (maxMm - distanceMm) / mmToCups;
-  //     Serial.print("Quantidade atual de xícaras: ");
-  //     Serial.println(actualQtdCup);
+    // Calculate the distance
+    // float distanceMm = ultrasonic.getDistanceMm();
+    // bool hasCup = cup.checkCup();
 
-  //     distanceMm = ultrasonic.getDistanceMm();
-  //     hasCup = cup.checkCup();
-      
-  //     delay(500);
-  //   }
-  // }else{
-  //   Serial.println("Não tem xícara");
-  //   digitalWrite(ledBuiltIn, LOW);
-  // }
+    // if(hasCup){
+    //   Serial.println("Tem xícara");
+    //   while(actualQtdCup < requestedCups && hasCup){
+    //     digitalWrite(ledBuiltIn, HIGH);
+    //     Serial.print("Distance (mm): ");
+    //     Serial.println(distanceMm);
 
-//   digitalWrite(portaRele, HIGH);
-//   delay(1500);
-//   digitalWrite(portaRele, LOW);
-  if(buttonRequest.wasPressed()){
-    Serial.println("Apertou o botão de request");
-  }
+    //     actualQtdCup = (maxMm - distanceMm) / mmToCups;
+    //     Serial.print("Quantidade atual de xícaras: ");
+    //     Serial.println(actualQtdCup);
 
-  if(buttonAddCup.wasPressed()){
-    Serial.println("Apertou o botão de addCup");
-    requestedCups++;
-    Serial.print("Quantidade de xícaras pedidas: ");
-    Serial.println(requestedCups);
-  }
+    //     distanceMm = ultrasonic.getDistanceMm();
+    //     hasCup = cup.checkCup();
 
-  // Serial.println("Nenhum botão foi apertado");
+    //     delay(500);
+    //   }
+    // }else{
+    //   Serial.println("Não tem xícara");
+    //   digitalWrite(ledBuiltIn, LOW);
+    // }
 
-  // delay(1000);
-}
-  // Prints the distance in the Serial Monitor
-  // Serial.print("Distance (mm): ");
-  // Serial.println(distanceMm);
-  Serial.println(cup.checkCup());
+    //   digitalWrite(portaRele, HIGH);
+    //   delay(1500);
+    //   digitalWrite(portaRele, LOW);
+    // if (buttonRequest.wasPressed()) {
+    //     Serial.println("Apertou o botão de request");
+    // }
 
-//   digitalWrite(portaRele, HIGH);
-//   delay(1500);
-//   digitalWrite(portaRele, LOW);
-  delay(1000);
+    if (buttonAddCup.wasPressed()) {
+        Serial.println("Apertou o botão de addCup");
+        requestedCups++;
+        Serial.print("Quantidade de xícaras pedidas: ");
+        Serial.println(requestedCups);
+    }
+
+    // Serial.println("Nenhum botão foi apertado");
+
+    // delay(1000);
 }
