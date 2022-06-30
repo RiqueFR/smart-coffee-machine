0b1b569656fc14a5987e9e2474fcfc3178e6930f
Henrique Faria Ribeiro <rique.far.rib@gmail.com>
2022-06-30 10:24:24 -0300

    Moved cup code to .h, to use with esphome

--- cup/cup.h
+++ cup/cup.h
@@ -1,14 +1,25 @@
 #ifndef CUP_H
 #define CUP_H
 
-#include <ultrasonic.h>
+#include "ultrasonic.h"
 
 class Cup : public Ultrasonic {
     
     public:
         Cup(int trigPin, int echoPin);
+            : Ultrasonic(trigPin, echoPin) {}
 
-        bool checkCup();
+        bool checkCup() {
+            float distance = this->getDistanceMm();
+
+            while(distance > 300){
+                distance = this->getDistanceMm();
+            }
+
+            return distance < 50;    
+        }
+
+        ~Cup(){}
 };
 
 #endif //CUP_H
