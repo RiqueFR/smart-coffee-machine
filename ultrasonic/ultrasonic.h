2b88479bee5f27718e05f3aceccf4afc3010b151
Henrique Faria Ribeiro <rique.far.rib@gmail.com>
2022-06-30 10:27:38 -0300

    Moved ultrasonic code to .h, to use with esphome

--- ultrasonic/ultrasonic.h
+++ ultrasonic/ultrasonic.h
@@ -1,7 +1,7 @@
 #ifndef ULTRASONIC_H
 #define ULTRASONIC_H
 
-#include <Arduino.h>
+#include "esphome.h"
 
 //define sound speed in mm/uS
 #define SOUND_SPEED 0.34
@@ -10,15 +10,36 @@ class Ultrasonic {
     private:
         int trigPin;
         int echoPin;
-    
+
     public:
-        Ultrasonic();
+        Ultrasonic() {};
 
-        Ultrasonic(int trigPin, int echoPin);
+        Ultrasonic(int trigPin, int echoPin) {
+            this->trigPin = trigPin;
+            this->echoPin = echoPin;
 
-        float getDistanceMm();
+            pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+            pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+        }
 
-        ~Ultrasonic();
+        float getDistanceMm() {
+            // Clears the trigPin
+            digitalWrite(trigPin, LOW);
+            delayMicroseconds(2);
+            // Sets the trigPin on HIGH state for 10 micro seconds
+            digitalWrite(trigPin, HIGH);
+            delayMicroseconds(10);
+            digitalWrite(trigPin, LOW);
+
+            // Reads the echoPin, returns the sound wave travel time in microseconds
+            double duration = pulseIn(echoPin, HIGH);
+
+            // Calculate the distance
+            return duration * SOUND_SPEED/2;
+        }
+
+
+        ~Ultrasonic(){}
 };
 
 #endif //ULTRASONIC_H
